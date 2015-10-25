83677931c4580e50df3afd89c5001f8d1d70b331
nasirkhan <nasir8891@gmail.com>
2015-10-25 16:02:42 +0600

    update asset links

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -10,17 +10,19 @@
         <meta name="description" content="@yield('title', config('settings.meta_description'))">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="apple-touch-icon" href="apple-touch-icon.png">
-        
-        {!! HTML::style('css/bootstrap.min.css') !!}
-        {!! HTML::style('css/main.css') !!}
-        
-        {!! HTML::script("js/vendor/modernizr-2.8.3-respond-1.4.2.min.js") !!}
+
+        <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+        <link rel="stylesheet" href="{{asset('css/main.css')}}">
+
+
+        <link rel="stylesheet" href="{{asset('js/vendor/modernizr-2.8.3-respond-1.4.2.min.js')}}">
+
 
     </head>
     <body>
         <!--[if lt IE 8]>
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
-        <![endif]-->
+            <![endif]-->
         <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
             <div class="container">
                 <div class="navbar-header">
@@ -54,9 +56,10 @@
                 <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
             </div>
         </div>
-<!--content area-->
-        @yield('content')
+
         <div class="container">
+            <!--content area-->
+            @yield('content')
             <!-- Example row of columns -->
             <div class="row">
                 <div class="col-md-4">
@@ -82,32 +85,29 @@
                 <p>&copy; Company 2015</p>
             </footer>
         </div> <!-- /container -->        
-        
+
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
 
-        {!! HTML::script("js/vendor/bootstrap.min.js") !!}
-        {!! HTML::script("js/plugins.js") !!}
-        {!! HTML::script("js/main.js") !!}
-        
+        <link rel="stylesheet" href="{{asset('js/vendor/bootstrap.min.js')}}">
+        <link rel="stylesheet" href="{{asset('js/plugins.js')}}">
+        <link rel="stylesheet" href="{{asset('js/main.js')}}">
 
-        <script src="js/plugins.js"></script>
-        <script src="js/main.js"></script>
 
         <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
         <script>
-            (function (b, o, i, l, e, r) {
-                b.GoogleAnalyticsObject = l;
-                b[l] || (b[l] =
-                        function () {
-                            (b[l].q = b[l].q || []).push(arguments)
-                        });
-                b[l].l = +new Date;
-                e = o.createElement(i);
-                r = o.getElementsByTagName(i)[0];
-                e.src = '//www.google-analytics.com/analytics.js';
-                r.parentNode.insertBefore(e, r)
-            }(window, document, 'script', 'ga'));
+                    (function (b, o, i, l, e, r) {
+                        b.GoogleAnalyticsObject = l;
+                        b[l] || (b[l] =
+                                function () {
+                                    (b[l].q = b[l].q || []).push(arguments)
+                                });
+                        b[l].l = +new Date;
+                        e = o.createElement(i);
+                        r = o.getElementsByTagName(i)[0];
+                        e.src = '//www.google-analytics.com/analytics.js';
+                        r.parentNode.insertBefore(e, r)
+                    }(window, document, 'script', 'ga'));
             ga('create', 'UA-XXXXX-X', 'auto');
             ga('send', 'pageview');
         </script>
