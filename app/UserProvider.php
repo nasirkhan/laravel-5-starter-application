58cc25b8a177a8ea9b3de85ef5ece2f452f6a62a
nasirkhan <nasir8891@gmail.com>
2015-11-16 11:42:50 +0600

    UserProvider belongsTo User

--- app/UserProvider.php
+++ app/UserProvider.php
@@ -7,19 +7,27 @@ use Illuminate\Database\Eloquent\Model;
 /**
  * User Provider Model
  */
-class UserProvider extends Model
-{
-    /**
-	 * The database table used by the model.
-	 *
-	 * @var string
-	 */
-	protected $table = 'user_providers';
+class UserProvider extends Model {
 
-	/**
-	 * The attributes that are not mass assignable.
-	 *
-	 * @var array
-	 */
-	protected $guarded = ['id'];
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'user_providers';
+
+    /**
+     * The attributes that are not mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = ['id'];
+
+    /**
+     * Get the user of a UserProvider
+     */
+    public function user() {
+        return $this->belongsTo('App\User');
+    }
+
 }
