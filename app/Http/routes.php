aca3bde57607f2b0f5ba8107d954906a72a5e860
nasirkhan <nasir8891@gmail.com>
2015-11-18 23:00:43 +0600

    fix

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,7 +14,7 @@
 Route::group(['namespace' => 'Frontend'], function () {
     Route::get('/', ['as' => 'home', 'uses' => 'FrontendController@index']);
     Route::get('home', ['as' => 'home', 'uses' => 'FrontendController@index']);
-    Route::get('dashboard', ['as' => 'home', 'uses' => 'DashboardController@index']);
+    Route::get('dashboard', ['as' => 'dashboard', 'uses' => 'DashboardController@index']);
 });
 
 // Authentication routes
