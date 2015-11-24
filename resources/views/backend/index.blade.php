ba47507109735e7d6c4c2277bc6a1aa6f9765d9b
nasirkhan <nasir8891@gmail.com>
2015-11-24 23:26:20 +0600

    new buttons for module

--- resources/views/backend/index.blade.php
+++ resources/views/backend/index.blade.php
@@ -25,4 +25,14 @@
   <i class="fa fa-users fa-2x pull-left"></i> All Users
 </a>
 
+
+<a class="btn btn-lg btn-success" href="admin/roles">
+  <i class="fa fa-user-secret fa-2x pull-left"></i> All Roles
+</a>
+
+
+<a class="btn btn-lg btn-success" href="admin/permissions">
+  <i class="fa fa-key fa-2x pull-left"></i> All Permissions
+</a>
+
 @endsection
