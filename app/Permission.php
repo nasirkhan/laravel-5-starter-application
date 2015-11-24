bcc01a805f1b0e44e974529d5c73f1143a1249b3
nasirkhan <nasir8891@gmail.com>
2015-11-25 01:07:33 +0600

    format

--- app/Permission.php
+++ app/Permission.php
@@ -4,17 +4,17 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Permission extends Model
-{
-    protected $guarded = ['id' ,'_token'];
-    
+class Permission extends Model {
+
+    protected $guarded = ['id', '_token'];
+
     /**
      * A permission can be applied to roles.
      *
      * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
      */
-    public function roles()
-    {
+    public function roles() {
         return $this->belongsToMany(Role::class);
     }
+
 }
