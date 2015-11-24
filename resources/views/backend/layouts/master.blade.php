a1971f1e3815d6af824957d34f0ae7f9f0a8a0d8
nasirkhan <nasir8891@gmail.com>
2015-11-24 14:50:19 +0600

    asset path update

--- resources/views/backend/layouts/master.blade.php
+++ resources/views/backend/layouts/master.blade.php
@@ -80,7 +80,7 @@
         </div> <!-- /container -->        
 
         <!--<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>-->
-        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.3.min.js"><\/script>')</script>
+        <script>window.jQuery || document.write('<script src="{{asset('js/vendor/jquery-1.11.3.min.js')}}"><\/script>')</script>
 
         <script src="{{asset('js/vendor/bootstrap.min.js')}}"></script>
         <script src="{{asset('js/plugins.js')}}"></script>
