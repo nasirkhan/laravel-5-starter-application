21e494e4a4b66f6e21e5de513db98a4e47939019
nasirkhan <nasir8891@gmail.com>
2015-11-24 12:16:15 +0600

    disabled CDN Call for development

--- resources/views/backend/layouts/master.blade.php
+++ resources/views/backend/layouts/master.blade.php
@@ -79,7 +79,7 @@
             </footer>
         </div> <!-- /container -->        
 
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <!--<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>-->
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.3.min.js"><\/script>')</script>
 
         <script src="{{asset('js/vendor/bootstrap.min.js')}}"></script>
