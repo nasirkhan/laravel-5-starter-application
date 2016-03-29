0aeb54fa819b606274df9d3d0908585b127f2912
nasirkhan <nasir8891@gmail.com>
2016-03-29 22:04:17 +0600

    master layout updated for Flash message

--- resources/views/backend/layouts/master.blade.php
+++ resources/views/backend/layouts/master.blade.php
@@ -21,6 +21,7 @@
 
     </head>
     <body>
+        @include('flash::message')
         <!--[if lt IE 8]>
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
