37c966cb6f8b3eff5627b3e967814a2be8e7c4c2
nasirkhan <nasir8891@gmail.com>
2016-03-31 17:06:33 +0600

    + app setting

--- config/settings.php
+++ config/settings.php
@@ -1,8 +1,13 @@
 <?php
 
 return [
-    'app_name'        => 'Laravel 5.1 Starter Application',
-    'meta_title'        => 'Laravel 5.1 Starter Application',
-    'meta_description'  => 'A Starter Application based on Laravel 5.1',
+    'app_name' => 'Laravel 5.1 Starter Application',
+    'meta_title' => 'Laravel 5.1 Starter Application',
+    'meta_description' => 'A Starter Application based on Laravel 5.1',
     
+    'admin' => [
+        'name' => env('ADMIN_NAME', 'Nasir Khan Saikat'),
+        'email' => env('ADMIN_EMAIL', 'no-reply@nasirkhn.com'),
+        'website' => env('ADMIN_WEBSITE', 'nasirkhn.com'),
+    ],
 ];
